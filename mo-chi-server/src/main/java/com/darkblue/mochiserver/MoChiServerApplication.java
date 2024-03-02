36668dd38ac0e3776be564df5f60bede25e9cbe3
Ag-epiphany <98681994+Ag-epiphany@users.noreply.github.com>
2024-03-03 01:01:18 +0800

    搭建了项目的初始框架(B/S)，标志项目正式开启。

---
+++ mo-chi-server/src/main/java/com/darkblue/mochiserver/MoChiServerApplication.java
@@ -0,0 +1,13 @@
+package com.darkblue.mochiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoChiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoChiServerApplication.class, args);
+	}
+
+}
---
+++ mo-chi-server/src/test/java/com/darkblue/mochiserver/MoChiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkblue.mochiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoChiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
